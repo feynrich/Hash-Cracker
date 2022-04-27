fc44f939fccf76845b05a8fcb96a675b8b1320f2
Aleksey Krokhin <askrokhin@miem.hse.ru>
2022-04-27 12:09:50 +0300

    main

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/agbel/CLionProjects/Hash-Cracker
+# Install script for directory: C:/Users/kroxi/CLionProjects/Hash-Cracker
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Hash_Cracker")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Hash-Cracker")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2021.3.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/agbel/CLionProjects/Hash-Cracker/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kroxi/CLionProjects/Hash-Cracker/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
-int pass_leng;
+#include <fstream>
+#include <string>
+#include <sstream>
 
-void generator(){
-    for (int i = 0; i<= pass_leng; i++){
 
+std::string hashfunc(std::string pass, std::string func) {
+
+}
+
+auto wordlist(std::string hash, std::string func) {
+    std::string path;
+
+    std::cin >> path;
+
+    std::string s;
+    std::string hashes;
+
+    std::ifstream file(path);
+
+    int i = 1;
+    while (std::getline(file, s)) {
+        hashes = hashfunc(s, func);
+        if (hashes == hash) {
+            std::cout << "Success! Password: " << s;
+            exit(0);
+        }
+    }
+    std::cout << "No match found.";
+    exit(0);
+}
+
+auto numlist(std::string hash, std::string func) {
+    int i = 0;
+    std::string hashes;
+
+    while (true) {
+        std::string str = std::to_string(i);
+        hashes = hashfunc(str, func);
+
+        if (hashes == hash) {
+                std::cout << "Success! Password: " << i;
+                exit(0);
+        }
     }
 }
 
-int main (){
-    std::cout << "loh;";
-    return 0;
+
+int main(int argc, char *argv[]) {
 
 }
+
 
