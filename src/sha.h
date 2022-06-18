2f35627e0194575dace736c68c5ff44d30d99288
Алексей Крохин <90411937+feynrich@users.noreply.github.com>
2022-06-18 15:43:29 +0300

    Update sha.h

--- src/sha.h
+++ src/sha.h
@@ -1,10 +1,9 @@
-
+#include <string>
 #ifndef HASH_CRACKER_SHA_H
 #define HASH_CRACKER_SHA_H
-#include <string>
-#include <vector>
-extern auto sha256(std::string pass) -> std::string;
-extern auto strtobin(std::string input) -> std::string;
-extern auto makebinaryhash(std::string pass) -> std::vector<std::bitset<32>>;
+extern auto sha256(std::string &pass) -> std::string;
+extern auto strtobin(std::string &input) -> std::string;
+extern auto makebinaryhash(std::string &pass) -> std::vector<std::bitset<32>>;
 extern auto extrawords(std::bitset<512> &message) -> std::vector<std::bitset<32>>;
 #endif //HASH_CRACKER_SHA_H
+
