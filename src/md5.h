982e87dc9e4ab87fc55277098f80b041869b429a
Алексей Крохин <90411937+feynrich@users.noreply.github.com>
2022-06-18 15:42:54 +0300

    Update md5.h

--- src/md5.h
+++ src/md5.h
@@ -1,16 +1,16 @@
 #include <string>
 #include <bitset>
-
+#include <vector>
 #ifndef HASH_CRACKER_MD5_H
 #define HASH_CRACKER_MD5_H
-extern auto md5(std::string input) -> std::string;
-extern auto make_little_endian(std::string message) -> std::string;
-extern auto little_endian(std::string input) -> std::string;
-extern auto FunG(std::bitset<32> x, std::bitset<32> y, std::bitset<32> z) -> std::bitset<32>;
-extern auto FunF(std::bitset<32> x, std::bitset<32> y, std::bitset<32> z) -> std::bitset<32>;
-extern auto FunH(std::bitset<32> x, std::bitset<32> y, std::bitset<32> z) -> std::bitset<32>;
-extern auto FunI(std::bitset<32> x, std::bitset<32> y, std::bitset<32> z) -> std::bitset<32>;
-extern auto ROTLEFT(std::bitset<32> x, int n) -> std::bitset<32>;
+extern auto md5(std::string &input) -> std::string;
+extern auto make_little_endian(std::string &message) -> std::string;
+extern auto little_endian(std::string &input) -> std::string;
+extern auto FunG(std::bitset<32> &x, std::bitset<32> &y, std::bitset<32> &z) -> std::bitset<32>;
+extern auto FunF(std::bitset<32> &x, std::bitset<32> &y, std::bitset<32> &z) -> std::bitset<32>;
+extern auto FunH(std::bitset<32> &x, std::bitset<32> &y, std::bitset<32> &z) -> std::bitset<32>;
+extern auto FunI(std::bitset<32> &x, std::bitset<32> &y, std::bitset<32> &z) -> std::bitset<32>;
+extern auto ROTLEFT(std::bitset<32> &x, int n) -> std::bitset<32>;
 extern auto MERGE_MD5(std::bitset<32> x, std::bitset<32> y) -> std::bitset<32>;
-extern auto strtobin_sec(std::string message) -> std::string;
+extern auto strtobin_sec(std::string &message) -> std::string;
 #endif //HASH_CRACKER_MD5_H
