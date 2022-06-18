2d3a52c368ea3b3434737eb79cbff1cec9e0f817
BAGstudent <90504857+BAGstudent@users.noreply.github.com>
2022-06-18 17:37:33 +0300

    Update gen.cpp

--- src/gen.cpp
+++ src/gen.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 #include <cstring>
 
-
+///alphabet
 std::string alphabet[62] = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
                             "J", "K", "L", "M", "N", "O", "P", "Q", "R",
                             "S", "T", "U", "V", "W", "X", "Y", "Z", "1",
